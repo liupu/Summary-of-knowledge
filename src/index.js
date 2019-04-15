4989b300d228d53d6394446189848557d066580f
liupu <178853518@qq.com>
2019-04-16 00:01:57 +0800

    update code for test env

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log(1);
+import React from 'react';
+import { render } from 'react-dom';
+const rootElement =  document.querySelector('#root');
+render(
+    <h1>Hello world</h1>,rootElement
+)
