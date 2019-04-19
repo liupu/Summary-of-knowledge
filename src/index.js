22b6206f57dabde1d5dd6242cb18e1d24362a242
liupu <178853518@qq.com>
2019-04-19 21:23:35 +0800

    update code for redux review

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
-const rootElement =  document.querySelector('#root');
-render(
-    <h1>Hello world</h1>,rootElement
-)
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import App from './components/app';
+import reducer from './reducer';
+const rootElement = document.querySelector('#root');
+const logger = createLogger();
+
+const store = createStore(
+    reducer,
+    applyMiddleware(logger));
+
+const render1 = () => {
+    render(
+        <App
+            value={store.getState()}
+            addFun={() => { store.dispatch({ type: 'ADD' }) }}
+            minusFun={() => { store.dispatch({ type: 'MINUS' }) }}
+            addAsNumFun={(num) => { store.dispatch({ type: 'ADDASNUM', num }) }}
+        />, rootElement
+    )
+}
+render1();
+store.subscribe(render1);
