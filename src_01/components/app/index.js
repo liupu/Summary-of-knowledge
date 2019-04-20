8847c5341fcc0c31dade5ec68ad0df6f5ecbfee9
liupu <178853518@qq.com>
2019-04-20 18:29:09 +0800

    justify code

--- src_01/components/app/index.js
+++ src_01/components/app/index.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 
-export default class App extends Component{
-    constructor(props){
+export default class App extends Component {
+    constructor(props) {
         super(props)
     }
-    addAsNum(){
+    addAsNum() {
         const inputNum = Number(this.refs.inputNum.value);
         this.props.addAsNumFun(inputNum);
         this.refs.inputNum.value = '';
     }
-    render(){
-        return(
+    render() {
+        const { value, addFun, minusFun } = this.props;
+        return (
             <div>
-                <p>{this.props.value}</p>
-                <button onClick={this.props.addFun}>+++</button>
+                <p>{value}</p>
+                <button onClick={addFun}>+++</button>
                 {" "}
-                <button onClick={this.props.minusFun}>---</button>
+                <button onClick={minusFun}>---</button>
                 {" "}
-                <input type='text' placeholder='Please input a number' ref='inputNum'/>
+                <input type='text' placeholder='Please input a number' ref='inputNum' />
                 {" "}
                 <button onClick={this.addAsNum.bind(this)}>Add the number</button>
             </div>
